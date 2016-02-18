64d880776105d0fd838a41978df2e1d890433bfd
Josh Olson <Olson.dev@Gmail.com>
2016-02-17 18:42:54 -0600

    Contact page now shows email/phone after reCAPTCHA
    Add TypeScript to project

---
+++ Client/ts/captcha.ts
@@ -0,0 +1,21 @@
+﻿function renderCaptcha() {
+	grecaptcha.render('grecaptcha', {
+		sitekey: '6LeIihgTAAAAAGJIaWSkdtXxZYiGY4y5ziVDOBbY'
+		, theme: 'dark'
+		, callback: (response: string) => {
+			const xhr = new XMLHttpRequest();
+			xhr.open('post', '/contact/info');
+			xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+			xhr.onload = () => {
+				if (xhr.status === 200) {
+					const obj = JSON.parse(xhr.responseText);
+					document.getElementById('email').textContent = obj.email;
+					document.getElementById('phone').textContent = obj.phone;
+				} else {
+					console.log(`Request failed; returned status ${xhr.status}.`);
+				}
+			};
+			xhr.send(encodeURI(`gRecaptchaResponse=${response}`));
+		}
+	});
+}
--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,10 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
 
 namespace PersonalWebApp.Controllers {
 	public class ContactController : Controller {
+		private readonly IConfiguration _configuration;
+
+		public ContactController(IConfiguration configuration) {
+			_configuration = configuration;
+		}
+		
 		public IActionResult Index() {
 			ViewData["Title"] = "Contact me";
 			return View();
+		}
+
+		public async Task<IActionResult> Info(string gRecaptchaResponse) {
+			var secret = _configuration["GRecaptchaSecret"];
+
+			var pairs = new Dictionary<string, string> { ["secret"] = secret, ["response"] = gRecaptchaResponse };
+			var httpContent = new FormUrlEncodedContent(pairs);
+			using (var client = new HttpClient()) {
+				client.BaseAddress = new Uri("https://www.google.com/");
+				var response = await client.PostAsync("recaptcha/api/siteverify", httpContent);
+				if (!response.IsSuccessStatusCode) return NoInfo();
+
+				var apiResponse = await response.Content.ReadAsStringAsync();
+				dynamic apiResponseObject = JObject.Parse(apiResponse);
+
+				var email = _configuration["Email"];
+				var phone = _configuration["Phone"];
+
+				return (bool)apiResponseObject.success
+					? Json(new { error = false, email, phone })
+					: Json(new { error = true, errors = apiResponseObject["error-codes"] })
+				;
+			}
+		}
+
+		private IActionResult NoInfo() {
+			return Json(new { error = true });
 		}
 	}
 }
