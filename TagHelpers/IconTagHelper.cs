a78cdfe23e2b331a19ade39f16a2b55dfe84c8a0
Josh Olson <Olson.dev@Gmail.com>
2016-02-18 19:24:09 -0600

    Create & use IconTagHelper
    IconTagHelper transforms <icon name="icon-on-layer-1,icon-on-layer-2"
    fill="#fff,#f00" size="48"/> into an inline <svg> with multiple <path>s
    Use several icons from https://materialdesignicons.com
    - Click icon, </> dropdown, View SVG, copy-paste full XML version to new
    file

---
+++ TagHelpers/IconTagHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using Microsoft.AspNet.Razor.TagHelpers;
+
+namespace PersonalWebApp.TagHelpers {
+	
+	[HtmlTargetElement("icon", TagStructure = TagStructure.WithoutEndTag, Attributes = "name")]
+	public class IconTagHelper : TagHelper {
+		public string Name { get; set; } = "alert";
+		public string Size { get; set; } = "24";
+		public string Fill { get; set; } = "fff";
+
+		public override void Process(TagHelperContext context, TagHelperOutput output) {
+			output.TagName = "svg";
+			output.TagMode = TagMode.StartTagAndEndTag;
+			output.Attributes["width"] = Size;
+			output.Attributes["height"] = Size;
+			output.Attributes["viewBox"] = "0 0 24 24";
+
+			var names = Name.Split(',');
+			var fills = Fill.Split(',');
+
+			for (var i = 0; i < names.Length; i++) {
+				var name = names[i];
+				var fill = i < fills.Length ? fills[i] : fills[fills.Length - 1];
+				output.Content.AppendHtml(GetPath(name, fill));
+			}
+		}
+
+		private string GetPath(string name, string fill) {
+			var doc = XDocument.Load($"Client/svg/{name}.svg");
+			var path = doc.Descendants(doc.Root.Name.Namespace + "path").First();
+			path.SetAttributeValue("fill", Regex.Replace(fill, "^#?", "#"));
+			return path.ToString(SaveOptions.DisableFormatting);
+		}
+	}
+}
