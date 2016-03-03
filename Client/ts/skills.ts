3e23b34bd2c39cfd5efbfac09e8366a37be4b804
Josh Olson <Olson.dev@Gmail.com>
2016-03-03 07:37:38 -0600

    Add support for searching by name
    DRYed out filter logic

--- Client/ts/skills.ts
+++ Client/ts/skills.ts
@@ -17,6 +17,24 @@ filterFormToggle.addEventListener('click', () => {
 
 let tags: string[] = [];
 
+function updateFilter(liElems: NodeListOf<Element>) {
+	const searchValue = search.value.trim().toLowerCase();
+	for (let i = 0; i < liElems.length; i++) {
+		const liElem = liElems[i] as HTMLLIElement;
+		liElem.classList.toggle('filtered', shouldBeFiltered(liElem, searchValue));
+	}
+}
+
+function shouldBeFiltered(liElem: HTMLLIElement, searchValue: string) {
+	for (const tag of tags) {
+		if (liElem.classList.contains(tag)) continue;
+		return true;
+	}
+	if (searchValue === '') return false;
+	const name = liElem.querySelector('h3').textContent.toLowerCase();
+	return name.indexOf(searchValue) === -1;
+}
+
 // Delegate click handler instead of listening on every element
 filterTags.addEventListener('click', (e) => {
 	if (!(e.target instanceof HTMLLIElement)) return;
@@ -25,33 +43,21 @@ filterTags.addEventListener('click', (e) => {
 	const tagResult = /(?:^| )(tag-\S+)/.exec(target.className);
 	const tag = tagResult.length ? tagResult[1] : '';
 	if (!tag) return;
-	
+
+	const elems = skills.querySelectorAll(`:not(.${tag})`);
 	if (tags.indexOf(tag) === -1) {
-		// Add to filter
 		tags.push(tag);
-		const elems = skills.querySelectorAll(`:not(.${tag})`);
 		for (let i = 0; i < elems.length; i++) {
 			elems[i].classList.add('filtered');
 		}
 	} else {
-		// Remove from filter
 		const index = tags.indexOf(tag);
 		tags.splice(index, 1);
-		const elems = skills.querySelectorAll(`:not(.${tag})`);
-		for (let i = 0; i < elems.length; i++) {
-			const elem = elems[i];
-			let hasEveryTag = true;
-			for (let otherTag of tags) {
-				if (!elem.classList.contains(otherTag)) {
-					hasEveryTag = false;
-					break;
-				}
-			}
-			if (!hasEveryTag) continue;
-			elem.classList.remove('filtered');
-		}
+		updateFilter(elems);
 	}
 });
+
+search.addEventListener('input', () => updateFilter(skills.children));
 
 clearFilters.addEventListener('click', () => {
 	search.value = '';
