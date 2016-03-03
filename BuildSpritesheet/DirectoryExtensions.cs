22acddc31d1d608cab6b894979f60ee38cb0d7c2
Josh Olson <Olson.dev@Gmail.com>
2016-03-03 15:22:26 -0600

    Create console app to spriteify skills images
    Currently exports resizes JPGs, PNGs, and SVGs
    It relies on Inkscape for SVGs
    - There is a caveat to that; the Inkscape CLI doesn't allow constraining
    width/height AND preserving aspect ratio. I'm guessing you probably
    could if you dug into its exhaustive list of "Verbs" but it's easier to
    just export a full detail PNG then resize that

---
+++ BuildSpritesheet/DirectoryExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace BuildSpritesheet {
+	public static class DirectoryExtensions {
+		/// <summary>
+		/// Helper method for when you need to get files with varied extensions
+		/// </summary>
+		/// <param name="path">The directory path</param>
+		/// <param name="extensionList">A list of extensions. Example: ".jpg,.png,.svg"</param>
+		/// <param name="searchOption">TopDirectoryOnly or AllDirectories</param>
+		/// <returns></returns>
+		public static IEnumerable<string> GetFilesByExtensionList(string path, string extensionList, SearchOption searchOption = SearchOption.TopDirectoryOnly) {
+			var regexPattern = $"({extensionList.Replace(',', '|')})$";
+      return GetFilesByRegex(path, regexPattern, searchOption);
+		}
+
+		public static IEnumerable<string> GetFilesByRegex(string path, string regexPattern = "", SearchOption searchOption = SearchOption.TopDirectoryOnly) {
+			var reSearchPattern = new Regex(regexPattern, RegexOptions.IgnoreCase);
+			return Directory.EnumerateFiles(path, "*", searchOption).Where(file => reSearchPattern.IsMatch(Path.GetFileName(file)));
+		}
+	}
+}
---
+++ BuildSpritesheet/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace BuildSpritesheet {
+	class Program {
+		static void Main(string[] args) {
+			Directory.CreateDirectory(GetOutputPath());
+
+			var logos = GetLogos();
+
+			foreach (var inputFilePath in logos) {
+				var ext = Path.GetExtension(inputFilePath)?.ToLower();
+				var outputFilePath = Path.ChangeExtension(CombinePath(GetOutputPath(), Path.GetFileName(inputFilePath)), "png");
+				switch (ext) {
+					case ".jpg":
+						ProcessJpg(inputFilePath, outputFilePath);
+						break;
+					case ".png":
+						ProcessPng(inputFilePath, outputFilePath);
+						break;
+					case ".svg":
+						ProcessSvg(inputFilePath, outputFilePath);
+						break;
+					default:
+						throw new NotImplementedException($"Cannot process file with extension ${ext}");
+				}
+			}
+
+			Console.WriteLine();
+			Console.WriteLine("Done processing individual files");
+			Console.ReadKey();
+		}
+
+		private static void ProcessJpg(string inputFilePath, string outputFilePath) {
+			Console.WriteLine($"Processing JPG: {Path.GetFileName(inputFilePath)}");
+			ProcessImage(inputFilePath, outputFilePath);
+		}
+
+		private static void ProcessPng(string inputFilePath, string outputFilePath) {
+			Console.WriteLine($"Processing PNG: {Path.GetFileName(inputFilePath)}");
+			ProcessImage(inputFilePath, outputFilePath);
+		}
+
+		private static void ProcessImage(string inputFilePath, string outputFilePath) {
+			using (var srcImg = Image.FromFile(inputFilePath)) {
+				using (var newImg = new Bitmap(60, 60)) {
+					using (var graphics = Graphics.FromImage(newImg)) {
+						graphics.SmoothingMode = SmoothingMode.HighQuality;
+						graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+						graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+						var dest = new Rectangle(0, 0, 60, 60);
+						if (srcImg.Width < srcImg.Height) {
+							dest.Width = (int)(60.0d * srcImg.Width / srcImg.Height);
+							dest.X = (60 - dest.Width) / 2;
+						} else {
+							dest.Height = (int)(60.0d * srcImg.Height / srcImg.Width);
+							dest.Y = (60 - dest.Height) / 2;
+						}
+
+						graphics.DrawImage(srcImg, dest);
+						srcImg.Dispose();
+						newImg.Save(outputFilePath, ImageFormat.Png);
+					}
+				}
+			}
+		}
+
+		private const string InkscapePath = @"C:\Storage\Inkscape\inkscape.exe";
+
+		private static void ProcessSvg(string inputFilePath, string outputFilePath) {
+			Console.WriteLine($"Processing SVG: {Path.GetFileName(inputFilePath)}");
+			var args = $"-z -f {inputFilePath} -e {outputFilePath}";
+			var inkscape = new Process {
+				StartInfo = {
+					UseShellExecute = false,
+					RedirectStandardOutput = true,
+					FileName = InkscapePath,
+					Arguments = args
+				}
+			};
+			inkscape.Start();
+			inkscape.WaitForExit();
+			ProcessImage(outputFilePath, outputFilePath);
+		}
+
+		private static IEnumerable<string> GetLogos() {
+			var sourcePath = GetSourcePath();
+			return DirectoryExtensions.GetFilesByExtensionList(sourcePath, ".jpg,.png,.svg");
+		}
+
+		static string GetBasePath() {
+			// e.g. C:\Storage\Projects\PersonalWebApp\BuildSpritesheet\bin\Debug\BuildSpritesheet.exe
+			var asmLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+			// e.g. C:\Storage\Projects\PersonalWebApp\
+			return CombinePath(asmLocation, "../../../../");
+		}
+
+		static string GetSourcePath() {
+			var basePath = GetBasePath();
+			return CombinePath(basePath, "Client/skills/");
+		}
+
+		static string GetOutputPath() {
+			var basePath = GetBasePath();
+			return CombinePath(basePath, "wwwroot/skills/");
+		}
+
+		static string CombinePath(params string[] paths) {
+			// Use GetFullPath to normalize with Path.DirectorySeparatorChar
+			return Path.GetFullPath(Path.Combine(paths));
+		}
+	}
+}
