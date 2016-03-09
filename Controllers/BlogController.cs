c9b8ca5b445b50c72253afbcf536f88cf2b1c181
Josh Olson <Olson.dev@Gmail.com>
2016-03-08 21:11:23 -0600

    Proof of concept code connects to Google OAuth2
    Will likely migrate code to TypeScript
    For now, just renders a "Sign in with Google" button
    When clicked, it sends a request to Google for email & profile
    (defaults)
    Upon success, it sends an XHR to the server to verify the token
    The server contacts Google, confirms the `aud` is our client ID
    If the `aud` is our client ID and the user is (me), send user details
    back to the browser

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -1,10 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
 
 namespace PersonalWebApp.Controllers {
 	public class BlogController : Controller {
+		private readonly IConfiguration _configuration;
+
+		public BlogController(IConfiguration configuration) {
+			_configuration = configuration;
+		}
 		public IActionResult Index() {
 			ViewData["Title"] = "My blog";
 			return View();
+		}
+
+		public IActionResult Admin() {
+			ViewData["Title"] = "My blog admin";
+			return View();
+		}
+
+		// ReSharper disable once InconsistentNaming
+		public async Task<IActionResult> Auth(string id_token) {
+			var pairs = new Dictionary<string, string> { ["id_token"] = id_token };
+			var httpContent = new FormUrlEncodedContent(pairs);
+			using (var client = new HttpClient()) {
+				client.BaseAddress = new Uri("https://www.googleapis.com/");
+				var response = await client.PostAsync("oauth2/v3/tokeninfo", httpContent);
+				if (!response.IsSuccessStatusCode) return NoInfo();
+				var apiResponse = await response.Content.ReadAsStringAsync();
+				dynamic apiResponseObject = JObject.Parse(apiResponse);
+
+				var googleClientId = _configuration["Data:GoogleClientId"];
+				var responseEmail = apiResponseObject.email.ToString().ToLower();
+				var validEmail = _configuration["Data:Email"].ToLower();
+				
+				return apiResponseObject.aud == googleClientId && responseEmail == validEmail
+					? Json(new {
+						error = false,
+						apiResponseObject.email,
+						apiResponseObject.picture,
+						apiResponseObject.name
+					})
+					: NoInfo()
+				;
+			}
+		}
+
+		private IActionResult NoInfo() {
+			return Json(new { error = true });
 		}
 	}
 }
