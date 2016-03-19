3945630af3ebb5614541c1a1f74f3a060947ad94
Josh Olson <Olson.dev@Gmail.com>
2016-03-19 17:21:46 -0500

    Properly use left outer join instead of inner join
    The caveat is EF7 RC1 has bugs currently that force you to pick between
    two evils:
    1. Call ToList(), effectively forcing double enumeration of the list
    (once in the service, once in the view; this is the lesser of the two
    evils)
    2. Don't call ToList(), effectively forcing EF to perform N+1 queries
    (bug)

--- Services/BlogService.cs
+++ Services/BlogService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Data.Entity;
 using PersonalWebApp.Models.Db;
 using Conceptual = PersonalWebApp.Models.Conceptual;
 
@@ -15,177 +16,59 @@ namespace PersonalWebApp.Services {
 		public Conceptual.BlogEntry GetById(string blogEntryId) => GetById(Guid.Parse(blogEntryId));
 
 		public Conceptual.BlogEntry GetById(Guid blogEntryId) {
-			var results =
-				from be in _dbContext.BlogEntries
-				join bc in _dbContext.BlogComments on be.EntryId equals bc.EntryId
-				where be.EntryId == blogEntryId
-				select new {
-					be.EntryId,
-					be.DatePublished,
-					be.DateCreated,
-					be.DateLastModified,
-					be.Title,
-					be.Slug,
-					EntryMarkdownContent = be.MarkdownContent,
-					EntryHtmlContent = be.HtmlContent,
+			var entry = _dbContext.BlogEntries.Include(m => m.Comments).Where(be => be.EntryId == blogEntryId).ToList().Select(bee => new Conceptual.BlogEntry {
+				EntryId = bee.EntryId,
+				DatePublished = bee.DatePublished,
+				DateCreated = bee.DateCreated,
+				DateLastModified = bee.DateLastModified,
+				Title = bee.Title,
+				Slug = bee.Slug,
+				MarkdownContent = bee.MarkdownContent,
+				HtmlContent = bee.HtmlContent,
+				Comments = bee.Comments.Select(bce => new Conceptual.BlogComment {
+					CommentId = bce.CommentId,
+					EntryId = bce.EntryId,
+					DateSubmitted = bce.DateSubmitted,
+					DateReviewed = bce.DateReviewed,
+					IsApproved = bce.IsApproved,
+					AuthorName = bce.AuthorName,
+					AuthorEmail = bce.AuthorEmail,
+					AuthorWebsite = bce.AuthorWebsite,
+					MarkdownContent = bce.MarkdownContent,
+					HtmlContent = bce.HtmlContent
+				}).ToList()
+			}).FirstOrDefault();
 
-					bc.CommentId,
-					bc.DateSubmitted,
-					bc.DateReviewed,
-					bc.IsApproved,
-					bc.AuthorName,
-					bc.AuthorEmail,
-					bc.AuthorWebsite,
-					CommentMarkdownContent = bc.MarkdownContent,
-					CommentHtmlContent = bc.HtmlContent,
-
-				}
-			;
-
-			var iter = results.GetEnumerator();
-			if (!iter.MoveNext()) {
+			if (entry == null) {
 				throw new InvalidOperationException($"BlogEntry with BlogEntryId {blogEntryId} does not exist");
 			}
-
-			var first = iter.Current;
-			var entry = new Conceptual.BlogEntry {
-				EntryId = first.EntryId,
-				DatePublished = first.DatePublished,
-				DateCreated = first.DateCreated,
-				DateLastModified = first.DateLastModified,
-				Title = first.Title,
-				Slug = first.Slug,
-				MarkdownContent = first.EntryMarkdownContent,
-				HtmlContent = first.CommentMarkdownContent,
-				Comments = new List<Conceptual.BlogComment>()
-			};
-
-			if (first.CommentId != Guid.Empty) {
-				entry.Comments.Add(new Conceptual.BlogComment() {
-					CommentId = first.CommentId,
-					EntryId = first.EntryId,
-					DateSubmitted = first.DateSubmitted,
-					DateReviewed = first.DateReviewed,
-					IsApproved = first.IsApproved,
-					AuthorName = first.AuthorName,
-					AuthorEmail = first.AuthorEmail,
-					AuthorWebsite = first.AuthorWebsite,
-					MarkdownContent = first.CommentMarkdownContent,
-					HtmlContent = first.CommentHtmlContent
-				});
-			}
-
-			while (iter.MoveNext()) {
-				var cur = iter.Current;
-				entry.Comments.Add(new Conceptual.BlogComment {
-					CommentId = cur.CommentId,
-					EntryId = cur.EntryId,
-					DateSubmitted = cur.DateSubmitted,
-					DateReviewed = cur.DateReviewed,
-					IsApproved = cur.IsApproved,
-					AuthorName = cur.AuthorName,
-					AuthorEmail = cur.AuthorEmail,
-					AuthorWebsite = cur.AuthorWebsite,
-					MarkdownContent = cur.CommentMarkdownContent,
-					HtmlContent = cur.CommentHtmlContent
-				});
-			}
-
+			
 			return entry;
 		}
 
 		public IEnumerable<Conceptual.BlogEntry> GetAll() {
-			var results =
-				from be in _dbContext.BlogEntries
-				join bc in _dbContext.BlogComments on be.EntryId equals bc.EntryId into bcs
-				from bc in bcs.DefaultIfEmpty()
-				orderby be.DatePublished descending, bc.DateSubmitted descending
-				select new {
-					be.EntryId,
-					be.DatePublished,
-					be.DateCreated,
-					be.DateLastModified,
-					be.Title,
-					be.Slug,
-					EntryMarkdownContent = be.MarkdownContent,
-					EntryHtmlContent = be.HtmlContent,
-
-					bc.CommentId,
-					bc.DateSubmitted,
-					bc.DateReviewed,
-					bc.IsApproved,
-					bc.AuthorName,
-					bc.AuthorEmail,
-					bc.AuthorWebsite,
-					CommentMarkdownContent = bc.MarkdownContent,
-					CommentHtmlContent = bc.HtmlContent,
-				}
-			;
-
-			using (var iter = results.GetEnumerator()) {
-				if (!iter.MoveNext()) yield break;
-
-				var cur = iter.Current;
-				var entry = new Conceptual.BlogEntry {
-					EntryId = cur.EntryId,
-					DatePublished = cur.DatePublished,
-					DateCreated = cur.DateCreated,
-					DateLastModified = cur.DateLastModified,
-					Title = cur.Title,
-					Slug = cur.Slug,
-					MarkdownContent = cur.EntryMarkdownContent,
-					HtmlContent = cur.CommentMarkdownContent,
-					Comments = new List<Conceptual.BlogComment>()
-				};
-
-				if (cur.CommentId != Guid.Empty) {
-					entry.Comments.Add(new Conceptual.BlogComment {
-						CommentId = cur.CommentId,
-						EntryId = cur.EntryId,
-						DateSubmitted = cur.DateSubmitted,
-						DateReviewed = cur.DateReviewed,
-						IsApproved = cur.IsApproved,
-						AuthorName = cur.AuthorName,
-						AuthorEmail = cur.AuthorEmail,
-						AuthorWebsite = cur.AuthorWebsite,
-						MarkdownContent = cur.CommentMarkdownContent,
-						HtmlContent = cur.CommentHtmlContent
-					});
-				}
-
-				while (iter.MoveNext()) {
-					cur = iter.Current;
-					if (cur.EntryId != entry.EntryId) {
-						yield return entry;
-						entry = new Conceptual.BlogEntry {
-							EntryId = cur.EntryId,
-							DatePublished = cur.DatePublished,
-							DateCreated = cur.DateCreated,
-							DateLastModified = cur.DateLastModified,
-							Title = cur.Title,
-							Slug = cur.Slug,
-							MarkdownContent = cur.EntryMarkdownContent,
-							HtmlContent = cur.CommentMarkdownContent,
-							Comments = new List<Conceptual.BlogComment>()
-						};
-					}
-					if (cur.CommentId != Guid.Empty) {
-						entry.Comments.Add(new Conceptual.BlogComment {
-							CommentId = cur.CommentId,
-							EntryId = cur.EntryId,
-							DateSubmitted = cur.DateSubmitted,
-							DateReviewed = cur.DateReviewed,
-							IsApproved = cur.IsApproved,
-							AuthorName = cur.AuthorName,
-							AuthorEmail = cur.AuthorEmail,
-							AuthorWebsite = cur.AuthorWebsite,
-							MarkdownContent = cur.CommentMarkdownContent,
-							HtmlContent = cur.CommentHtmlContent
-						});
-					}
-				}
-				yield return entry;
-			}
+			return _dbContext.BlogEntries.Include(m => m.Comments).ToList().Select(bee => new Conceptual.BlogEntry {
+				EntryId = bee.EntryId,
+				DatePublished = bee.DatePublished,
+				DateCreated = bee.DateCreated,
+				DateLastModified = bee.DateLastModified,
+				Title = bee.Title,
+				Slug = bee.Slug,
+				MarkdownContent = bee.MarkdownContent,
+				HtmlContent = bee.HtmlContent,
+				Comments = bee.Comments.Select(bce => new Conceptual.BlogComment {
+					CommentId = bce.CommentId,
+					EntryId = bce.EntryId,
+					DateSubmitted = bce.DateSubmitted,
+					DateReviewed = bce.DateReviewed,
+					IsApproved = bce.IsApproved,
+					AuthorName = bce.AuthorName,
+					AuthorEmail = bce.AuthorEmail,
+					AuthorWebsite = bce.AuthorWebsite,
+					MarkdownContent = bce.MarkdownContent,
+					HtmlContent = bce.HtmlContent
+				}).ToList()
+			});
 		}
 	}
 }
