4dfc405ba7b837bb7cfd9abc67a83959aa0f0929
Josh Olson <Olson.dev@Gmail.com>
2016-03-22 02:43:48 -0500

    Scaffold a blog admin form
    No validation, submission, anything like that yet
    - Sign in with Google button now hides after signing in
    - Also shows "Save" and "Update preview" buttons

--- Client/ts/blog-admin.ts
+++ Client/ts/blog-admin.ts
@@ -1,19 +1,32 @@
 ﻿'use strict';
 
+const signin = document.getElementById('signin');
+const buttons = document.querySelectorAll('.buttons button');
+
+function toggleButtons(show: boolean) {
+	for (let i = 0; i < buttons.length; i++) {
+		(buttons[i] as HTMLButtonElement).hidden = !show;
+	}
+}
+
+toggleButtons(false);
+
 function onSignIn(googleUser: gapi.auth2.GoogleUser) {
 	const idToken = googleUser.getAuthResponse().id_token;
 	var xhr = new XMLHttpRequest();
 	xhr.open('POST', '/blog/auth');
 	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 	xhr.onload = () => {
-	if (xhr.status === 200) {
-		const obj = JSON.parse(xhr.responseText);
-		console.log(`Signed in as ${obj.name}`);
-		console.log(`Picture URL: ${obj.picture}`);
-		console.log(`Email: ${obj.email}`);
-	} else {
-		console.log(`Request failed; returned status ${xhr.status}.`);
-	}
+		if (xhr.status === 200) {
+			const obj = JSON.parse(xhr.responseText);
+			console.log(`Signed in as ${obj.name}`);
+			console.log(`Picture URL: ${obj.picture}`);
+			console.log(`Email: ${obj.email}`);
+			signin.hidden = true;
+			toggleButtons(true);
+		} else {
+			console.log(`Request failed; returned status ${xhr.status}.`);
+		}
 	};
 	xhr.send(`id_token=${idToken}`);
 }
@@ -24,12 +37,12 @@ function onFailure(error: any) {
 
 function renderButton() {
 	gapi.signin2.render('signin', {
-	scope: 'profile email',
-	width: 240,
-	height: 50,
-	longtitle: true,
-	theme: 'dark',
-	onsuccess: onSignIn,
-	onfailure: onFailure
+		scope: 'profile email',
+		width: 240,
+		height: 50,
+		longtitle: true,
+		theme: 'dark',
+		onsuccess: onSignIn,
+		onfailure: onFailure
 	});
 }
